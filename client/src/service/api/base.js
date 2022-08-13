3a14d4e857ebc0d3001c8388d265b5e2de027528
xunyao4dev <87527853+xunyao4dev@users.noreply.github.com>
2022-08-13 13:36:42 +0800

    Delete client/src directory

--- client/src/service/api/base.js
+++
@@ -1,8 +0,0 @@
-import Request from '../../utils/request';
-
-export default class base {
-  static get    = Request.get;
-  static put    = Request.put;
-  static post   = Request.post;
-  static delete = Request.del;
-}
--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import common from './common'
-import auth from './auth'
-
-
-export default {
-  common,
-  auth,
-}
